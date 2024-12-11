7ce359d9677f4c4a6ac0219dce60b6c086f21459
rafahuelin <rafahuelin@gmail.com>
2024-12-11 15:42:51 +0100

    format on save

--- api/src/main.py
+++ api/src/main.py
@@ -1,11 +1,11 @@
 """Main package for the API."""
 
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root() -> dict:
-    """"Return a greeting."""
+    """Return a greeting."""
     return {"Hello": "World"}
